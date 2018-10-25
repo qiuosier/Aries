2207f516025b9cb40e435876d97e822ccb1c82a1
Qiu <qiuosier@gmail.com>
2018-10-25 11:47:18 -0400

    Add strings module.

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import re
 import string
 
 
@@ -15,3 +16,15 @@ def remove_non_alphanumeric(s):
         if (c in string.digits or c in string.ascii_letters)
     ])
     return new_str
+
+
+def remove_escape_sequence(s):
+    """Removes ANSI escape sequences, including color codes.
+
+    Args:
+        s (str): A string
+
+    Returns: A string with escape sequence removed.
+
+    """
+    return re.sub(r"\x1b\[.*m", "", s)
