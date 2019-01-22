db79fb2749c9b8925655ccc9f214e1d1821fc0e8
Qiu <qiuosier@gmail.com>
2019-01-22 11:08:30 -0500

    Update FileName class and move it to strings module.

--- tests/test_strings.py
+++ tests/test_strings.py
@@ -1,6 +1,10 @@
+import sys
+from os.path import dirname
+sys.path.append(dirname(dirname(dirname(__file__))))
+
 import unittest
 import logging
-from Aries.strings import AString
+from Aries.strings import AString, FileName
 logger = logging.getLogger(__name__)
 
 
@@ -18,12 +22,16 @@ class TestAString(unittest.TestCase):
             attr = t[1]
             expected = t[2]
             x = AString(value)
-            self.assertEqual(getattr(x, attr)(), expected, "Test Failed. Attribute: %s" % attr)
+            actual = getattr(x, attr)()
+            self.assertEqual(type(actual), AString)
+            self.assertEqual(str(actual), expected, "Test Failed. Attribute: %s" % attr)
 
         # Test split() method, which returns a list
         arr = AString("test string").split(" ")
         self.assertTrue(isinstance(arr, list), "split() should return a list.")
         self.assertEqual(len(arr), 2, "split() should return a list of 2 elements.")
+        self.assertEqual(type(arr[0]), AString)
+        self.assertEqual(type(arr[1]), AString)
         self.assertEqual(arr[0], "test", "split() Error.")
         self.assertEqual(arr[1], "string", "split() Error.")
 
@@ -40,4 +48,27 @@ class TestAString(unittest.TestCase):
 
 
 class TestFileName(unittest.TestCase):
-    pass
+    def test_filename_class(self):
+        input_string = "abc.def"
+        filename = FileName(input_string)
+        self.assertEqual(type(filename), FileName)
+        self.assertEqual(str(filename), input_string)
+
+        basename = filename.basename
+        self.assertEqual(str(basename), "abc")
+
+        extension = filename.extension
+        self.assertEqual(str(extension), "def")
+
+        title = filename.title()
+        self.assertEqual(type(title), FileName)
+        self.assertEqual(str(title), "Abc.def")
+        self.assertEqual(str(filename.upper()), "ABC.def")
+
+        # Test appending random string
+        n = 5
+        filename_with_random = filename.title().append_random_letters(n)
+        # len will only count the length of basename
+        self.assertEqual(len(filename_with_random), len(input_string) + n - 3)
+        self.assertIn(title.basename, str(filename_with_random))
+        self.assertIn(title.extension, str(filename_with_random))
