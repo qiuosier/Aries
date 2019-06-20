824840940ceb93645e15479c639b149132a5aaad
Qiu <qiuosier@gmail.com>
2019-06-19 22:37:23 -0400

    Test: Add logger_names option to test logging.

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ from collections import OrderedDict
 from unittest import TestCase
 
 
-ARIES_LOGGING_FORMAT = '%(asctime)s | %(levelname)-8s | %(lineno)4d@%(module)-12s | %(message)s'
+ARIES_LOGGING_FORMAT = '%(asctime)s | %(levelname)-8s | %(lineno)4d@%(module)-15s | %(message)s'
 ARIES_LOGGING_DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
 
 
@@ -16,6 +16,10 @@ class AriesTest(TestCase):
         1. A stream handler is added to the root logger to output debug messages.
         2. Test outputs are buffered. Outputs for passing tests are discard.
             Only outputs for failed tests will be displayed.
+
+        By default, the steam handler will be added to the root logger.
+        This can be changed by override the "logger_names" class attribute 
+            to a list of logger names, e.g. logger_names = ["tests"].
 
     Usage:
         This class can be used to replace unittest.TestCase.
@@ -30,19 +34,39 @@ class AriesTest(TestCase):
     # value: time elapsed during the test.
     times = OrderedDict()
 
+    # Override logger_names in subclasses to avoid enabling debug logging for root logger.
+    logger_names = [""]
+
     @staticmethod
-    def enable_logging():
+    def new_stream_handler():
+        """Initialize a new stream handler.
+        
+        Returns: A StreamHandler.
+        """
         stream_handler = logging.StreamHandler(sys.stdout)
         stream_handler.setFormatter(
             logging.Formatter(ARIES_LOGGING_FORMAT, ARIES_LOGGING_DATE_FORMAT)
         )
-        logger = logging.getLogger()
-        logger.setLevel(logging.DEBUG)
-        logger.addHandler(stream_handler)
-        logger.debug("Debug logging is enabled.")
+        return stream_handler
 
     @staticmethod
-    def __decorate_test_case(func):
+    def enable_debug_logging(name):
+        """Sets logging level to debug and add a stream_handler to format the logging outputs.
+        
+        Args:
+            name (str): logger name.
+        
+        Returns: the logger.
+        """
+        stream_handler = AriesTest.new_stream_handler()
+        logger = logging.getLogger(name)
+        logger.setLevel(logging.DEBUG)
+        logger.addHandler(stream_handler)
+        logger.debug("Debug Logging Enabled.")
+        return logger
+
+    @classmethod
+    def __decorate_test_case(cls, func):
         """A decorator for test case.
         1. Adds additional logging handler to test case so that logging will be streamed to stdout.
         2. Set logger level to DEBUG
@@ -54,19 +78,20 @@ class AriesTest(TestCase):
 
         """
         def test_case_with_logging(*args, **kwargs):
-            stream_handler = logging.StreamHandler(sys.stdout)
-            stream_handler.setFormatter(
-                logging.Formatter(ARIES_LOGGING_FORMAT, ARIES_LOGGING_DATE_FORMAT)
-            )
-            logger = logging.getLogger()
-            level = logger.getEffectiveLevel()
-            logger.setLevel(logging.DEBUG)
-            logger.addHandler(stream_handler)
+            stream_handler = AriesTest.new_stream_handler()
+            loggers = dict()
+            for name in cls.logger_names:
+                logger = logging.getLogger(name)
+                level = logger.getEffectiveLevel()
+                loggers[logger] = level
+                logger.setLevel(logging.DEBUG)
+                logger.addHandler(stream_handler)
             try:
                 results = func(*args, **kwargs)
             finally:
-                logger.removeHandler(stream_handler)
-                logger.setLevel(level)
+                for logger, level in loggers.items():
+                    logger.removeHandler(stream_handler)
+                    logger.setLevel(level)
             return results
 
         return test_case_with_logging
