7efc836ae5ce95ddb78bc65389e4e5667997d3e8
Qiu <qiuosier@gmail.com>
2020-03-18 10:59:34 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 import setuptools
+import os
+from Aries.files import Markdown
 
-with open("README.md", "r") as fh:
+with open("Aries/README.md", "r") as fh:
     long_description = fh.read()
+    long_description = Markdown.from_text(long_description).make_links_absolute("https://github.com/qiuosier/Aries/blob/master/")
+
+version = os.popen('cd Aries && git rev-list --count master').read()
 
 setuptools.setup(
     name="Astrology-Aries",
-    version="0.0.1.dev1",
+    version="0.0.1.dev%s" % version,
     author="Qiu Qin",
     author_email="qiuosier@gmail.com",
-    description="The Aries Library",
+    description="Python package providing shortcuts to tasks like "
+                "accessing files on the cloud, running background tasks, configuring logging, etc.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/qiuosier/Aries",
