84dd02ffb612c108b7d71333862ae54f6774d219
Qiu <qiuosier@gmail.com>
2020-06-23 11:36:22 -0400

    Remove gcp.storage.py

--- gcp/storage.py
+++
@@ -1,6 +0,0 @@
-"""This module has been moved to Aries.storage package.
-The imports in this files are for backward compatibility purpose.
-Use Aries.storage module for new developments.
-This file will be removed in the future.
-"""
-from ..storage.gs import GSObject, GSFolder, GSFile
